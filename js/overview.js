f48bd0fc8b24d6edb8cee9957409cd7e02f739b7
Richard You <richardyou94@gmail.com>
2022-08-17 07:22:43 -0700

    finished creating one that can be saved in local storage

--- js/overview.js
+++ js/overview.js
@@ -1,59 +1,3 @@
-// variables that will be used in code
-// var saveBtn = $('.saveBtn');
-// var notiBox = $('.notification-box');
-
-// this below is the array of sets of array.. 
-// var setOfTimeSchedules = [];
-// function createSetsOfArray () {
-// for(var i=1; i <= 100; i++) {
-//     setOfTimeSchedules['testing' + i] = [];
-// }};
-// createSetsOfArray();
-// console.log(setOfTimeSchedules);
-// console.log("hello")
-
-
-// Real Code is Below
-// $(document).ready(function () {
-//     saveBtn.on('click', function () {
-//         var value = $(this).siblings('.description').val();
-//         var startingTime = $(this).siblings('.startingTime').val();
-//         var endingTime = $(this).siblings('.endingTime').val();
-        
-//     localStorage.setItem(value, startingTime, endingTime); 
-
-    
-//     })
-// })
-// Real Code is Above
-
-
-
-//Testing Code is Below////////////////////////////////////////////////////////////////////////////////////////////////
-// var value = 'descriptoin 1 for schedule'
-// var startingTime = '9:00AM'
-// var endingTime = '10:00AM'
-
-// setOfTimeSchedules.push('schedule'+(setOfTimeSchedules.length + 1)) 
-
-// console.log(setOfTimeSchedules)
-// setOfTimeSchedules[setOfTimeSchedules.length].push("test")
-// console.log(setOfTimeSchedule[0])
-
-// var schedule1
-// var schedule2
-
-
-// .button-save < save button
-// .time-starting < starting time.$
-// .time-ending < ending time
-// .description-schedule < description of the schedule
-// .date-selected < date 
-
-// .status-todo < routine. priority. urgent
-// .description-todo < descirpiont what to do 
-// .due-time < 
-// .due-date < 
 
 // whole container of the schedule box
 var scheduleContainerEl = $('#schedule-container');
@@ -66,79 +10,194 @@ var descriptionScheduleInputEl = $('#description-schedule');
 var submitBtnEl = $('#button-submit');
 
 var scheduleFormEl = $('#project-form');
-// .addClass('row-test');
-function printSchedule(dueDate, start, end, description) {
-    var scheduleRowEl = $('<div>');
-        var timeBoxEl = $('<div>');
-            var startTimeEl = $('<h6>');
-            var lineBetween = $('<hr>');
-            var endTimeEl = $('<h6>');
-        var descriptionBoxEl = $('<div>');
-            var descSchedule = $('<h5>');
-        var editBtn = $('<button>');
-        var cancelBtn = $('<button>');
-        console.log(dueDate);
+
+var scheduleListForAppend = [];
+
+function renderLocalStorage() {
+    scheduleContainerEl.empty();
+    console.log(scheduleListForAppend);
+    if (typeof scheduleListForAppend.length == "undefined") {
+        for (var i = 0; i < 1; i++) {
+            // console.log(scheduleListForAppend.startingTime);
+    console.log("undefined: i will become 1");
+            var dateParsed = scheduleListForAppend.dates;
+            var startTimeParsed = scheduleListForAppend.startingTime;
+            var endTimeParsed = scheduleListForAppend.endingTime;
+            var schedulesParsed = scheduleListForAppend.schedules;
+            
+            // printSchedule(dateParsed, startTimeParsed, endTimeParsed, schedulesParsed);
+            var scheduleRowEl = $('<div>');
+            var timeBoxEl = $('<div>');
+                var startTimeEl = $('<h6>');
+                var lineBetween = $('<hr>');
+                var endTimeEl = $('<h6>');
+            var descriptionBoxEl = $('<div>');
+                var descSchedule = $('<h5>');
+            var editBtn = $('<button>');
+            var cancelBtn = $('<button>');
+            // console.log(dueDate);
+            
+    
+        scheduleRowEl.addClass('row-test');
+            timeBoxEl.addClass('time-container');
+                startTimeEl.addClass('starting-time');
+                endTimeEl.addClass('ending-time');
+            descriptionBoxEl.addClass('description-container');
+            editBtn.addClass('button-edit');
+            cancelBtn.addClass('button-cancel');
+    
+                startTimeEl.text(startTimeParsed);
+                endTimeEl.text(endTimeParsed);
+                descSchedule.text(schedulesParsed);
+                editBtn.text("edit");
+                cancelBtn.text("cancel");
+    
+        scheduleRowEl.attr('data-dates', dateParsed);
+        scheduleRowEl.attr('data-index', i);
+            // console.log("printing?");
+            
+            timeBoxEl.append(startTimeEl);
+            timeBoxEl.append(lineBetween);
+            timeBoxEl.append(endTimeEl);
+    
+            descriptionBoxEl.append(descSchedule);
+    
+        scheduleRowEl.append(timeBoxEl);
+        scheduleRowEl.append(descriptionBoxEl);
+        scheduleRowEl.append(editBtn);
+        scheduleRowEl.append(cancelBtn);
+    
+        scheduleContainerEl.append(scheduleRowEl);
+        }
+    } else {
+        for (var i = 0; i < scheduleListForAppend.length; i++) {
+            // console.log(scheduleListForAppend.startingTime);
+    console.log(scheduleListForAppend.length);
+            var dateParsed = scheduleListForAppend[i].dates
+            var startTimeParsed = scheduleListForAppend[i].startingTime;
+            var endTimeParsed = scheduleListForAppend[i].endingTime;
+            var schedulesParsed = scheduleListForAppend[i].schedules;
+            
+            // printSchedule(dateParsed, startTimeParsed, endTimeParsed, schedulesParsed);
+            var scheduleRowEl = $('<div>');
+            var timeBoxEl = $('<div>');
+                var startTimeEl = $('<h6>');
+                var lineBetween = $('<hr>');
+                var endTimeEl = $('<h6>');
+            var descriptionBoxEl = $('<div>');
+                var descSchedule = $('<h5>');
+            var editBtn = $('<button>');
+            var cancelBtn = $('<button>');
+            // console.log(dueDate);
+            
+    
+        scheduleRowEl.addClass('row-test');
+            timeBoxEl.addClass('time-container');
+                startTimeEl.addClass('starting-time');
+                endTimeEl.addClass('ending-time');
+            descriptionBoxEl.addClass('description-container');
+            editBtn.addClass('button-edit');
+            cancelBtn.addClass('button-cancel');
+    
+                startTimeEl.text(startTimeParsed);
+                endTimeEl.text(endTimeParsed);
+                descSchedule.text(schedulesParsed);
+                editBtn.text("edit");
+                cancelBtn.text("cancel");
+    
+        scheduleRowEl.attr('data-dates', dateParsed);
+        scheduleRowEl.attr('data-index', i);
+            // console.log("printing?");
+            
+            timeBoxEl.append(startTimeEl);
+            timeBoxEl.append(lineBetween);
+            timeBoxEl.append(endTimeEl);
+    
+            descriptionBoxEl.append(descSchedule);
+    
+        scheduleRowEl.append(timeBoxEl);
+        scheduleRowEl.append(descriptionBoxEl);
+        scheduleRowEl.append(editBtn);
+        scheduleRowEl.append(cancelBtn);
+    
+        scheduleContainerEl.append(scheduleRowEl);
+// sorting with time
         
-
-    scheduleRowEl.addClass('row-test');
-        timeBoxEl.addClass('time-container');
-            startTimeEl.addClass('starting-time');
-            endTimeEl.addClass('ending-time');
-        descriptionBoxEl.addClass('description-container');
-        editBtn.addClass('button-edit');
-        cancelBtn.addClass('button-cancel');
-
-            startTimeEl.text(start);
-            endTimeEl.text(end);
-            descSchedule.text(description);
-            editBtn.text("edit");
-            cancelBtn.text("cancel");
-
-    scheduleRowEl.attr('data-dates', dueDate);
-        console.log("printing?");
-        
-        timeBoxEl.append(startTimeEl);
-        timeBoxEl.append(lineBetween);
-        timeBoxEl.append(endTimeEl);
-
-        descriptionBoxEl.append(descSchedule);
-
-    scheduleRowEl.append(timeBoxEl);
-    scheduleRowEl.append(descriptionBoxEl);
-    scheduleRowEl.append(editBtn);
-    scheduleRowEl.append(cancelBtn);
-
-    scheduleContainerEl.append(scheduleRowEl);
-
-// need hide function after submitted    
-console.log(scheduleContainerEl)
+    // sorting end
+}};
 }
-//handle delete schedule function
-function handleDeleteSchedule(event) {
-    var btnClicked = $(event.target);
-    btnClicked.parent('div').remove();
+function init() {
+    var storedSchedulesInit = JSON.parse(localStorage.getItem("scheduleList"));
+
+    if (storedSchedulesInit !== null) {
+        var storedSchedules = storedSchedulesInit;
+        scheduleListForAppend = storedSchedules;
+    }
+
+
+    renderLocalStorage();
 }
+function saveToLocalStorage() {
+   
+    localStorage.setItem("scheduleList", JSON.stringify(scheduleListForAppend));
 
-// handle schedule form submission
-
+}
 function handleScheduleFormSubmit(event) {
     event.preventDefault();
-console.log("submit!?")
+
     var scheduleDate = scheduleDateInputEl.val();
     var scheduleStartTime = startingTimeInputEl.val();
     var scheduleEndTime = endingTimeInputEl.val();
     var scheduleDescription = descriptionScheduleInputEl.val();
 
-    printSchedule(scheduleDate, scheduleStartTime, scheduleEndTime, scheduleDescription);
+    // return when description is empty
+    if (scheduleDescription === "") {
+        return;
+    }
 
+    var scheduleItem = {
+        dates: scheduleDate,
+        startingTime: scheduleStartTime,
+        endingTime: scheduleEndTime,
+        schedules: scheduleDescription
+    };
+
+    if (scheduleListForAppend.length === 0) {
+        scheduleListForAppend = scheduleItem;
+        console.log("empty")
+    } else if (typeof scheduleListForAppend.length == "undefined") {
+        scheduleListForAppend = [scheduleListForAppend]
+        scheduleListForAppend.push(scheduleItem);
+        console.log(scheduleListForAppend);
+    } else {
+        console.log("third time?")
+        scheduleListForAppend.push(scheduleItem);
+        console.log(scheduleListForAppend);
+    }
+
+    
+    // endingTime: scheduleEndTime, schedules: scheduleDescription});
     scheduleFormEl[0].reset();
-console.log("submitted");
+    // printSchedule(scheduleDate, scheduleStartTime, scheduleEndTime, scheduleDescription);
+    saveToLocalStorage();
+    renderLocalStorage();
+    
+    
+
 }
-// submitBtnEl.on('click', function() {
-//     handleScheduleFormSubmit;
-//     console.log("submit");
-// })
+function handleDeleteSchedule(event) {
+    var btnClicked = $(event.target);
+    btnClicked.parent('div').remove();
+
+    if (btnClicked.matches(".button-cancel") === true) {
+        var index = btnClicked.parentElement.getAttribute("data-index");
+        scheduleListForAppend.splice(index, 1);
+
+        saveToLocalStorage();
+        renderLocalStorage();
+    }
+}
 scheduleFormEl.on('submit', handleScheduleFormSubmit);
 scheduleContainerEl.on('click', '.button-cancel', handleDeleteSchedule);
 
-
+init();
