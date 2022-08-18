7e219a162f067358197420dcd88f6ca9a101f2e1
Richard You <richardyou94@gmail.com>
2022-08-18 08:34:56 -0700

    moment js applied to the calendar, schedule.

--- js/overview.js
+++ js/overview.js
@@ -1,4 +1,97 @@
-var currentDate = moment().format("YYYY-MM-DD")
+///////////////////////////////////////// Schedule ////////////////////////////////////////////////
+var today = moment();
+var currentDate = today.format("YYYY-MM-DD");
+$('#today-date').text(today.format("dddd, MMMM D, YYYY"));
+var currentDayOfWeek = today.format("dddd");
+var daySunday = $('#week-sunday');
+var dayMonday = $('#week-monday');
+var dayTuesday = $('#week-tuesday');
+var dayWednesday = $('#week-wednesday');
+var dayThursday = $('#week-thursday');
+var dayFriday = $('#week-friday');
+var daySaturday = $('#week-saturday');
+
+findDayOfWeek();
+function findDayOfWeek() {
+    if (currentDayOfWeek == "Sunday") {
+        console.log("Sunday");
+        daySunday.addClass('darkgrey');
+        daySunday.text(today.format("D"))
+        dayMonday.text(today.add(1, 'd').format("D"));
+        dayTuesday.text(today.add(1, 'd').format("D"));
+        dayWednesday.text(today.add(1, 'd').format("D"));
+        dayThursday.text(today.add(1, 'd').format("D"));
+        dayFriday.text(today.add(1, 'd').format("D"));
+        daySaturday.text(today.add(1, 'd').format("D"));
+    } else if (currentDayOfWeek == "Monday") {
+        console.log("Monday");
+        dayMonday.addClass('darkgrey');
+        daySunday.text(today.subtract(1, 'd').format("D"));
+        dayMonday.text(today.add(1,'d').format("D"));
+        dayTuesday.text(today.add(1, 'd').format("D"));
+        dayWednesday.text(today.add(1, 'd').format("D"));
+        dayThursday.text(today.add(1, 'd').format("D"));
+        dayFriday.text(today.add(1, 'd').format("D"));
+        daySaturday.text(today.add(1, 'd').format("D"));
+    } else if (currentDayOfWeek == "Tuesday") {
+        console.log("Tuesday");
+        dayTuesday.addClass('darkgrey');
+        daySunday.text(today.subtract(2, 'd').format("D"));
+        dayMonday.text(today.add(1,'d').format("D"));
+        dayTuesday.text(today.add(1, 'd').format("D"));
+        dayWednesday.text(today.add(1, 'd').format("D"));
+        dayThursday.text(today.add(1, 'd').format("D"));
+        dayFriday.text(today.add(1, 'd').format("D"));
+        daySaturday.text(today.add(1, 'd').format("D"));
+    } else if (currentDayOfWeek == "Wednesday") {
+        console.log("Wednesday");
+        dayWednesday.addClass('darkgrey');
+        daySunday.text(today.subtract(3, 'd').format("D"));
+        dayMonday.text(today.add(1,'d').format("D"));
+        dayTuesday.text(today.add(1, 'd').format("D"));
+        dayWednesday.text(today.add(1, 'd').format("D"));
+        dayThursday.text(today.add(1, 'd').format("D"));
+        dayFriday.text(today.add(1, 'd').format("D"));
+        daySaturday.text(today.add(1, 'd').format("D"));
+    } else if (currentDayOfWeek == "Thursday") {
+        console.log("Thursday");
+        dayThursday.addClass('darkgrey');
+        daySunday.text(today.subtract(4, 'd').format("D"));
+        dayMonday.text(today.add(1,'d').format("D"));
+        dayTuesday.text(today.add(1, 'd').format("D"));
+        dayWednesday.text(today.add(1, 'd').format("D"));
+        dayThursday.text(today.add(1, 'd').format("D"));
+        dayFriday.text(today.add(1, 'd').format("D"));
+        daySaturday.text(today.add(1, 'd').format("D"));
+    } else if (currentDayOfWeek == "Friday") {
+        console.log("Friday");
+        dayFriday.addClass('darkgrey');
+        daySunday.text(today.subtract(5, 'd').format("D"));
+        dayMonday.text(today.add(1,'d').format("D"));
+        dayTuesday.text(today.add(1, 'd').format("D"));
+        dayWednesday.text(today.add(1, 'd').format("D"));
+        dayThursday.text(today.add(1, 'd').format("D"));
+        dayFriday.text(today.add(1, 'd').format("D"));
+        daySaturday.text(today.add(1, 'd').format("D"));
+    } else if (currentDayOfWeek == "Saturday") {
+        console.log("Saturday");
+        daySaturday.addClass('darkgrey');
+        daySunday.text(today.subtract(6, 'd').format("D"));
+        dayMonday.text(today.add(1,'d').format("D"));
+        dayTuesday.text(today.add(1, 'd').format("D"));
+        dayWednesday.text(today.add(1, 'd').format("D"));
+        dayThursday.text(today.add(1, 'd').format("D"));
+        dayFriday.text(today.add(1, 'd').format("D"));
+        daySaturday.text(today.add(1, 'd').format("D"));
+    } else {
+        console.log("getting error");
+    }
+}
+
+
+
+// $('#week-sunday').text(moment().format("D"));
+
 // whole container of the schedule box
 var scheduleContainerEl = $('#schedule-container');
 // variable declared for input details
@@ -71,66 +164,62 @@ function renderLocalStorage() {
         }
     } else {
         for (var i = 0; i < scheduleListForAppend.length; i++) {
-
-            
-            
             scheduleListForAppend.sort((a, b) => a.timeStamp - b.timeStamp);
             
-            // if (scheduleListForAppend[i].dates !== currentDate) {
-            //     return;
-            // }
-
-            var dateParsed = scheduleListForAppend[i].dates
-            var startTimeParsed = scheduleListForAppend[i].startingTime;
-            var endTimeParsed = scheduleListForAppend[i].endingTime;
-            var schedulesParsed = scheduleListForAppend[i].schedules;
-            
-            // printSchedule(dateParsed, startTimeParsed, endTimeParsed, schedulesParsed);
-            var scheduleRowEl = $('<div>');
-            var timeBoxEl = $('<div>');
-                var startTimeEl = $('<h6>');
-                var lineBetween = $('<hr>');
-                var endTimeEl = $('<h6>');
-            var descriptionBoxEl = $('<div>');
-                var descSchedule = $('<h5>');
-            var editBtn = $('<button>');
-            var cancelBtn = $('<button>');
-            // console.log(dueDate);
-            
-    
-        scheduleRowEl.addClass('row-test');
-            timeBoxEl.addClass('time-container');
-                startTimeEl.addClass('starting-time');
-                endTimeEl.addClass('ending-time');
-            descriptionBoxEl.addClass('description-container');
-            editBtn.addClass('button-edit');
-            cancelBtn.addClass('button-cancel');
-    
-                startTimeEl.text(startTimeParsed);
-                endTimeEl.text(endTimeParsed);
-                descSchedule.text(schedulesParsed);
-                editBtn.text("edit");
-                cancelBtn.text("cancel");
-    
-        scheduleRowEl.attr('data-dates', dateParsed);
-        scheduleRowEl.attr('data-index', i);
-            // console.log("printing?");
-            
-            timeBoxEl.append(startTimeEl);
-            timeBoxEl.append(lineBetween);
-            timeBoxEl.append(endTimeEl);
-    
-            descriptionBoxEl.append(descSchedule);
-    
-        scheduleRowEl.append(timeBoxEl);
-        scheduleRowEl.append(descriptionBoxEl);
-        scheduleRowEl.append(editBtn);
-        scheduleRowEl.append(cancelBtn);
-    
-        scheduleContainerEl.append(scheduleRowEl);
-// sorting with time
+            if (scheduleListForAppend[i].dates !== currentDate) {
+                console.log("skip!");
+            } else {
+                var dateParsed = scheduleListForAppend[i].dates
+                var startTimeParsed = scheduleListForAppend[i].startingTime;
+                var endTimeParsed = scheduleListForAppend[i].endingTime;
+                var schedulesParsed = scheduleListForAppend[i].schedules;
+                
+                // printSchedule(dateParsed, startTimeParsed, endTimeParsed, schedulesParsed);
+                var scheduleRowEl = $('<div>');
+                var timeBoxEl = $('<div>');
+                    var startTimeEl = $('<h6>');
+                    var lineBetween = $('<hr>');
+                    var endTimeEl = $('<h6>');
+                var descriptionBoxEl = $('<div>');
+                    var descSchedule = $('<h5>');
+                var editBtn = $('<button>');
+                var cancelBtn = $('<button>');
+                // console.log(dueDate);
+                
         
-    // sorting end
+            scheduleRowEl.addClass('row-test');
+                timeBoxEl.addClass('time-container');
+                    startTimeEl.addClass('starting-time');
+                    endTimeEl.addClass('ending-time');
+                descriptionBoxEl.addClass('description-container');
+                editBtn.addClass('button-edit');
+                cancelBtn.addClass('button-cancel');
+        
+                    startTimeEl.text(startTimeParsed);
+                    endTimeEl.text(endTimeParsed);
+                    descSchedule.text(schedulesParsed);
+                    editBtn.text("edit");
+                    cancelBtn.text("cancel");
+        
+            scheduleRowEl.attr('data-dates', dateParsed);
+            scheduleRowEl.attr('data-index', i);
+                // console.log("printing?");
+                
+                timeBoxEl.append(startTimeEl);
+                timeBoxEl.append(lineBetween);
+                timeBoxEl.append(endTimeEl);
+        
+                descriptionBoxEl.append(descSchedule);
+        
+            scheduleRowEl.append(timeBoxEl);
+            scheduleRowEl.append(descriptionBoxEl);
+            scheduleRowEl.append(editBtn);
+            scheduleRowEl.append(cancelBtn);
+        
+            scheduleContainerEl.append(scheduleRowEl);
+            }
+
+
 }};
 }
 function init() {
@@ -211,20 +300,3 @@ scheduleContainerEl.on('click', '.button-cancel', handleDeleteSchedule);
 
 init();
 
-
-
-
-// const items = [
-//     { name: 'Edward', value: 21 },
-//     { name: 'Sharpe', value: 37 },
-//     { name: 'And', value: 45 },
-//     { name: 'The', value: -12 },
-//     { name: 'Magnetic', value: 13 },
-//     { name: 'Zeros', value: 37 }
-// ]
-// console.log(items)
-// items.sort((a,b) => a.value - b.value);
-// console.log(items)
-// console.log(scheduleListForAppend)
-// console.log(scheduleListForAppend[0].dates)
-// console.log(currentDate)
